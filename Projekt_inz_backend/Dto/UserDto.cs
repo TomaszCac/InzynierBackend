9cb775000fc140746a13e49ffc6904641fbf36a7
Tomasz Cac <tomaszandrzej1234@gmail.com>
2024-01-07 13:25:24 +0100

    Added role to user output

--- Projekt_inz_backend/Dto/UserDto.cs
+++ Projekt_inz_backend/Dto/UserDto.cs
@@ -6,5 +6,6 @@
         public string username { get; set; }
         public string? password { get; set; }
         public string? email { get; set; }
+        public string? role { get; set; }
     }
 }
